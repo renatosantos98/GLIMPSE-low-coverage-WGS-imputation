bcf4929455368ce0d6c1d79fd2e538e2163ae8ea
Renato Santos <renato.santos98@hotmail.com>
2022-10-16 08:52:32 +0100

    Added ancilliary files. Fixed concordance script.

--- 5_glimpse_concordance.sh
+++ 5_glimpse_concordance.sh
@@ -1,6 +1,9 @@
+# Change to use a different number of CPU threads.
+threads=16
+
 # Results validation
-GLIMPSE_concordance --thread 16 --input GLIMPSE_concordance/concordance.lst --minDP 8 --output GLIMPSE_concordance/output --minPROB 0.9999 --bins 0.00000 0.00100 0.00200 0.00500 0.01000 0.05000 0.10000 0.20000 0.50000
+GLIMPSE_concordance --thread $threads --input GLIMPSE_concordance/concordance.lst --minDP 8 --output GLIMPSE_concordance/output --minPROB 0.9999 --bins 0.00000 0.00100 0.00200 0.00500 0.01000 0.05000 0.10000 0.20000 0.50000
 
 # Requires file concordance.lst with the following parameters
 # 'Region' 'Allele frequencies at each site' 'The validation 30x dataset' 'The imputed data'
-chr22 1000G/1000G.chr22.sites.vcf.gz vcf/30x/manuel_corpas/chr22.vcf.gz GLIMPSE_ligated/manuel_corpas/chr22.merged.bcf
+# chr22 1000G/1000G.chr22.sites.vcf.gz vcf/30x/manuel_corpas/chr22.vcf.gz GLIMPSE_ligated/manuel_corpas/chr22.merged.bcf
