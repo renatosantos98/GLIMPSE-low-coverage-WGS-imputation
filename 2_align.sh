1647521942941527b91cf34cd04086783a03d635
Renato Santos <renato.santos98@hotmail.com>
2022-10-17 11:14:29 +0100

    Changed the alignment script to use paired-end reads

--- 2_align.sh
+++ 2_align.sh
@@ -9,17 +9,23 @@ sortthreads=`expr $threads / 2`
 # Change 1x to 30x for high-coverage files.
 coverage=1x
 
-mkdir -p seq/1x
+mkdir -p seq/${coverage}
 
-for finput in fastq/*.fastq.gz;
-    do samoutput=${finput%.fastq.gz}.sam;
-    bamoutput=${samoutput#fastq/};
+for fastq1 in fastq/22*1PFW/clean_data/22*_1.fq.gz;
+    do fastq2=fastq/22*1PFW/clean_data/22*_2.fq.gz;
+    echo fastq1: ${fastq1};
+    echo fastq2: ${fastq2};
+    samoutput=${fastq1#"fastq/"};
+    samoutput=${samoutput%"/clean_data/"22*_1.fq.gz}.sam;
+    echo sam: ${samoutput};
+    bamoutput=${samoutput#"fastq/"};
     bamoutput=seq/${coverage}/${bamoutput%.sam}.bam;
-    echo Aligning $finput...;
-    bwa mem -t $threads reference/hg19.fa $finput > $samoutput;
-    echo Sorting $samoutput...;
+    echo bam: ${bamoutput};
+    echo Aligning ${samoutput%.sam}...;
+    bwa mem -t $threads reference/hg19.fa $fastq1 $fastq2 > $samoutput;
+    echo Converting and sorting $samoutput to bam format...;
     samtools view --threads $sortthreads -b -h --fai-reference reference/hg19.fa.fai $samoutput | samtools sort --threads $sortthreads -o $bamoutput;
     rm -f $samoutput;
     echo Indexing $bamoutput...;
-    samtools index --threads $threads $bamoutput;
+    samtools index -@ $threads $bamoutput;
 done
